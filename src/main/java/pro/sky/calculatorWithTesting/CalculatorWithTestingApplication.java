0b0db5a14cab7486995e7efa6c29ca2fc278c789
Евгений Страхов <goodwin.s@mail.ru>
2025-01-31 01:13:47 +0300

    Initial commit

---
+++ src/main/java/pro/sky/calculatorWithTesting/CalculatorWithTestingApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorWithTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWithTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorWithTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/calculatorWithTesting/CalculatorWithTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculatorWithTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWithTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
